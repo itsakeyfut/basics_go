eba9b799e99a3b06a983a930f71f170922d809ef
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-05-04 04:26:45 +0900

    add multiple functions

---
+++ playground/fn/multiple_fn.go
@@ -0,0 +1,19 @@
+package fn
+
+import "strings"
+
+func GetInitials(n string) (string, string) {
+	s := strings.ToUpper(n)
+	names := strings.Split(s, " ")
+
+	var initials []string
+	for _, v := range names {
+		initials = append(initials, v[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[1]
+	}
+
+	return initials[0], "_"
+}
--- playground/main.go
+++ playground/main.go
@@ -30,12 +30,19 @@ func main() {
 	// Logical Operators and continue and break keyword in for looping
 	// loop_n_conditional.LogicalOperators()
 
-	// Function
-	fn.CycleNames([]string{"cloud", "tifa", "barret"}, fn.SayGreeting)
-	fn.CycleNames([]string{"cloud", "tifa", "barret"}, fn.SayBye)
+	// // Function
+	// fn.CycleNames([]string{"cloud", "tifa", "barret"}, fn.SayGreeting)
+	// fn.CycleNames([]string{"cloud", "tifa", "barret"}, fn.SayBye)
 
-	a1 := fn.CircleArea(10.5)
-	a2 := fn.CircleArea(15)
-	fmt.Println(a1, a2)
-	fmt.Printf("Circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
+	// a1 := fn.CircleArea(10.5)
+	// a2 := fn.CircleArea(15)
+	// fmt.Println(a1, a2)
+	// fmt.Printf("Circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
+
+	// Multiple Functions
+	org, upper := fn.GetInitials("Akari Akaza")
+	fmt.Printf("%v is upper to %v \n", org, upper)
+
+	org2, upper2 := fn.GetInitials("Mirakurun")
+	fmt.Printf("%v is upper to %v \n", org2, upper2)
 }
